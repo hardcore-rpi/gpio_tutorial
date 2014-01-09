ed48b33dd1187053f483eb132c59d79080600980
concefly <concefly@foxmail.com>
2014-01-09 06:05:55 +0000

    Update pwm.py

--- basic/pwm/pwm.py
+++ basic/pwm/pwm.py
@@ -1,18 +1,22 @@
+import RPIO
 from RPIO import PWM
 import time
 
 # With the default subcycle time of 20ms and default pulse-width increment granularity of 10us
 servo = PWM.Servo()
 
-RPIO.setmode(RPIO.BOARD)
-pin = 11
+# Only GPIO id
+pin = 17
 
 try:
-	# Set servo on pin to 1200us(1.2ms)
-	servo.set_servo(pin, 1200)
+	# Set servo on pin to 1000us(1ms,5%)
+	servo.set_servo(pin, 1000)
 	time.sleep(2)
-	# Set servo on pin to 5000us(5.0ms)
+	# Set servo on pin to 5000us(5.0ms,25%)
 	servo.set_servo(pin, 5000)
+	time.sleep(2)
+	# Set servo on pin to 10000us(10ms,50%)
+	servo.set_servo(pin, 10000)
 	time.sleep(2)
 except KeyboardInterrupt:
 	print 
