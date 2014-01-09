bb7e59684d3b183f3b9f11e27dd46597859e9a5a
concefly <concefly@foxmail.com>
2014-01-09 21:17:12 +0000

    Debug blink (sysfs)

--- sysfs/blink/main.c
+++ sysfs/blink/main.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <signal.h>
 
-#define PIN 17
+#define PIN_STR "17"
+#define PIN_INT 17
 
 int loop_run = 1;
 
@@ -13,30 +14,40 @@ void loop_stop(int sig)
 
 int main()
 {
-	FILE fd=NULL;
+	FILE * fd;
+
+	signal(SIGINT,loop_stop);
 	
-	fd = open("/sys/class/gpio/export",'w');
-	fputs(#PIN,fd);
+	fd = fopen("/sys/class/gpio/export","w");
+	fputs(PIN_STR,fd);
 	fclose(fd);
 	
-	printf("export gpio%d\n",PIN);
+	printf("export gpio%d\n",PIN_INT);
 	
-	fd = open("/sys/class/gpio/gpio" #PIN,'w');
+	fd = fopen("/sys/class/gpio/gpio" PIN_STR "/direction","w");
+	fputs("out",fd);
+	fclose(fd);
+
+	fd = fopen("/sys/class/gpio/gpio" PIN_STR "/value","w");
 	while(loop_run)
 	{
 		fputs("1",fd);
-		printf("\rGPIO%d = 1",PIN);
+		printf("\rGPIO%d = 1",PIN_INT);
+		fflush(stdout);
+		fflush(fd);
 		sleep(1);
 		fputs("0",fd);
-		printf("\rGPIO%d = 0",PIN);
+		printf("\rGPIO%d = 0",PIN_INT);
+		fflush(stdout);
+		fflush(fd);
 		sleep(1);
 	}
 	fclose(fd);
 	
-	fd = open("/sys/class/gpio/unexport",'w');
-	fputs(#PIN,fd);
+	fd = fopen("/sys/class/gpio/unexport","w");
+	fputs(PIN_STR,fd);
 	fclose(fd);
-	printf("unexport gpio%d\n",PIN);
+	printf("unexport gpio%d\n",PIN_INT);
 	
 	return 0;
 }
