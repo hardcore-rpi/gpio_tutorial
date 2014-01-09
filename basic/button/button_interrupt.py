d3bdc9725570857a54807f3acfcac476ef899b45
concefly <concefly@foxmail.com>
2014-01-09 05:41:16 +0000

    Update button_interrupt.py

--- basic/button/button_interrupt.py
+++ basic/button/button_interrupt.py
@@ -1,12 +1,12 @@
 import RPIO
 
-pin = 11
+# Only GPIO id
+pin = 17
 
 def gpio_callback(gpio_id, val):
 	print "gpio %s = %s" %(gpio_id, val)
 
-RPIO.setmode(RPIO.BOARD)
-RPIO.add_interrupt_callback(pin, gpio_callback, edge='rising', pull_up_down=RPIO.PUD_DOWN)
+RPIO.add_interrupt_callback(pin, gpio_callback, edge='both', pull_up_down=RPIO.PUD_DOWN)
 
 try:
 	RPIO.wait_for_interrupts()
