d82ad493d68a9497dbb4e7f0b74a7cdb8aa91973
concefly <concefly@foxmail.com>
2014-07-02 16:09:29 +0800

    Replace pin with gpio_id in some files

--- basic/button/button_interrupt.py
+++ basic/button/button_interrupt.py
@@ -1,12 +1,12 @@
 import RPIO
 
 # Only GPIO id
-pin = 17
+gpio_id = 17
 
 def gpio_callback(gpio_id, val):
 	print "gpio %s = %s" %(gpio_id, val)
 
-RPIO.add_interrupt_callback(pin, gpio_callback, edge='both', pull_up_down=RPIO.PUD_DOWN)
+RPIO.add_interrupt_callback(gpio_id, gpio_callback, edge='both', pull_up_down=RPIO.PUD_DOWN)
 
 try:
 	RPIO.wait_for_interrupts()
--- basic/pwm/pwm.py
+++ basic/pwm/pwm.py
@@ -6,21 +6,21 @@ import time
 servo = PWM.Servo()
 
 # Only GPIO id
-pin = 17
+gpio_id = 17
 
 try:
-	# Set servo on pin to 1000us(1ms,5%)
-	servo.set_servo(pin, 1000)
+	# Set servo on gpio_id to 1000us(1ms,5%)
+	servo.set_servo(gpio_id, 1000)
 	time.sleep(2)
-	# Set servo on pin to 5000us(5.0ms,25%)
-	servo.set_servo(pin, 5000)
+	# Set servo on gpio_id to 5000us(5.0ms,25%)
+	servo.set_servo(gpio_id, 5000)
 	time.sleep(2)
-	# Set servo on pin to 10000us(10ms,50%)
-	servo.set_servo(pin, 10000)
+	# Set servo on gpio_id to 10000us(10ms,50%)
+	servo.set_servo(gpio_id, 10000)
 	time.sleep(2)
 except KeyboardInterrupt:
 	print 
 	print 'Exit'
 finally:
-	# Clear servo on pin
-	servo.stop_servo(pin)
+	# Clear servo on gpio_id
+	servo.stop_servo(gpio_id)
